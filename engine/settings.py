2bca44a283b5b5d2a06d9c333cb7b28978a9743c
fcoouto <f.couto@live.com>
2023-06-28 00:42:32 +0200

    Update settings.py

--- engine/settings.py
+++ engine/settings.py
@@ -184,7 +184,7 @@ BROKERS = {
                 'context': 'tv',
                 'id': 'navbar_ema_settings',
                 'region': None,
-                'locate_confidence': 0.75,
+                'locate_confidence': 0.70,
             },
             'ema_settings_tab1': {
                 'context': 'tv',
