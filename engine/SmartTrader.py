87b9c83b8a1fc02dbc8857cdaf8f4e5ffe7de4ed
fcoouto <f.couto@live.com>
2023-06-28 21:14:53 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -476,7 +476,7 @@ class SmartTrader:
                     right = width
                     bottom = height * 0.87
                 elif element_id == 'ema_72':
-                    left = width * 0.48
+                    left = width * 0.475
                     top = height * 0.89
                     right = width * 0.70
                     bottom = height
