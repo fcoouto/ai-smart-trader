558122bec4f5032bf4f52764492f68a081f2d280
fcoouto <f.couto@live.com>
2023-07-02 07:55:16 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -1659,7 +1659,7 @@ class SmartTrader:
         # Appending data to [positions.csv] file
         positions_file = os.path.join(settings.PATH_DATA, 'positions.csv')
         df = self.df_ongoing_positions()
-        positions = df.query(f'df.Strategy == "{strategy_id}"')
+        positions = df.query(f'Strategy == "{strategy_id}"')
         positions.to_csv(positions_file, mode='a', index=False, header=False)
 
         self.position_history.append(self.ongoing_positions[strategy_id].copy())
