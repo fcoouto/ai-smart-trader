67b42e010b2ed32391ac35a6315028cb5f14f171
fcoouto <f.couto@live.com>
2023-07-03 21:20:57 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -803,15 +803,17 @@ class SmartTrader:
         if value > self.highest_balance:
             self.highest_balance = value
 
-        if value > self.balance * (1 + self.stop_loss_pct):
-            # New value is greater than expected.
-            msg = (f"{tmsg.warning}[WARNING]{tmsg.endc} "
-                   f"{tmsg.italic}- Seems like Your current Balance is [{value} USD], "
-                   f"which is way greater than last reading: [{self.balance} USD]. {tmsg.endc}")
-            tmsg.print(msg=msg, clear=True)
 
-            msg = f"{tmsg.italic}\n\t- Could you confirm if I read it right? {tmsg.endc}"
-            tmsg.input(msg=msg)
+        if self.balance:
+            if value > self.balance * (1 + self.stop_loss_pct):
+                # New value is greater than expected.
+                msg = (f"{tmsg.warning}[WARNING]{tmsg.endc} "
+                       f"{tmsg.italic}- Seems like Your current Balance is [{value} USD], "
+                       f"which is way greater than last reading: [{self.balance} USD]. {tmsg.endc}")
+                tmsg.print(msg=msg, clear=True)
+
+                msg = f"{tmsg.italic}\n\t- Could you confirm if I read it right? {tmsg.endc}"
+                tmsg.input(msg=msg)
 
         self.balance = value
         return self.balance
