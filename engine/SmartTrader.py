3482185ef30bc554620fe1323b952e1dbf54b439
fcoouto <f.couto@live.com>
2023-07-07 21:44:29 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -262,7 +262,7 @@ class SmartTrader:
         delta = now - app_now
         tries = 0
 
-        while abs(delta.total_seconds()) > 1:
+        while abs(delta.total_seconds()) > 1.5:
             # Out of sync confirmed
 
             wait_secs = 3
