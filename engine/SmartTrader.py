80f9d1b14a9367a6085b16e32c585b07d959f12b
fcoouto <f.couto@live.com>
2023-07-11 17:12:32 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -1700,7 +1700,8 @@ class SmartTrader:
     def playbook_activate_super_strike(self):
         # Activating [super_Strike]
         self.click_element(element_id='btn_super_strike', wait_when_done=0.500)
-        self.click_element(element_id='btn_activate', duration=0.200, wait_when_done=0.250)
+        self.move_to_element(element_id='btn_activate')
+        self.click_element(element_id='btn_activate')
 
         # Leaving [super_strike] menu
         self.mouse_event_on_neutral_area(area_id='screen_center_25')
