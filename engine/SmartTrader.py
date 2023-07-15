fec0bea4235e34a988b480505424bcb752ba6b6e
fcoouto <f.couto@live.com>
2023-07-15 09:29:23 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -1794,9 +1794,16 @@ class SmartTrader:
     def playbook_activate_super_strike(self):
         # Activationg [super_Strike] mode
         self.click_element(element_id='btn_super_strike', wait_when_done=2)
-        pyautogui.click()
-        self.click_element(element_id='btn_activate', wait_when_done=2)
-        sleep(1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
+        self.click_element(element_id='btn_activate', wait_when_done=1)
 
         # Leaving [super_strike] menu
         self.mouse_event_on_neutral_area(area_id='screen_center_25')
