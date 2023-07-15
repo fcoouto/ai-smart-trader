e68c3091bfad5e5cf99a05ed6c3da83ae11024f5
fcoouto <f.couto@live.com>
2023-07-15 08:54:10 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -1794,7 +1794,7 @@ class SmartTrader:
     def playbook_activate_super_strike(self):
         # Activationg [super_Strike] mode
         self.click_element(element_id='btn_super_strike', wait_when_done=2)
-        self.click_element(element_id='btn_activate', wait_when_done=0.250)
+        self.click_element(element_id='btn_activate', wait_when_done=0.500)
         pyautogui.press('space')
 
         # Leaving [super_strike] menu
