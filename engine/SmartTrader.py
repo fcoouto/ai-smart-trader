0501b660c0aa071c39f0683e17742c20e6ee4076
fcoouto <f.couto@live.com>
2023-07-31 15:30:41 +0200

    Update SmartTrader.py

--- engine/SmartTrader.py
+++ engine/SmartTrader.py
@@ -2134,7 +2134,6 @@ class SmartTrader:
         r = requests.post(url=request_url,
                           headers=headers,
                           data=data)
-        print(r.text)
         r = json.loads(r.text)
 
         for k, v in r.items():
