9b51dd34081cb3458b427f0566578cb705a2b9d3
fcoouto <f.couto@live.com>
2023-09-24 18:19:54 +0200

    Update settings.py

--- engine/settings.py
+++ engine/settings.py
@@ -13,7 +13,7 @@ MIN_BALANCE = 100
 MIN_PAYOUT = 70
 #   CHART_DATA_SECONDS is about [ohlc, ema, rsi] values that should be stored in a list.
 #   They must represent the closing of a candle.
-CHART_DATA_MIN_SECONDS = 58
+CHART_DATA_MIN_SECONDS = 56
 CHART_DATA_MAX_SECONDS = 5
 CHART_DATA_READING_LIMIT_SECONDS = 5
 REFRESH_PAGE_EVERY_RUN = 1
